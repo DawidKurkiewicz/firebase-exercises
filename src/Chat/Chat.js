a06a84001459e123e634fd46ab7f4c5303b395c7
Mateusz Choma <chomamateusz@gmail.com>
2018-11-28 14:50:40 +0100

    Maping object to array

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -1,22 +1,47 @@
 import React from 'react'
 
+import { database } from '../firebaseConfig'
+
 import NewMessageForm from './NewMessageForm'
+
+const dbMessagesRef = database.ref('/jfddl6-messages')
 
 class Chat extends React.Component {
   state = {
     newMessageText: 'krowa'
   }
 
+  componentDidMount() {
+    dbMessagesRef.on(
+      'value',
+      snapshot =>
+        console.log(
+          Object.entries(
+            snapshot.val()
+          ).map(entry => ({
+            ...entry[1],
+            key: entry[0]
+          }))
+        )
+    )
+  }
+
   onNewMessageTextChangeHandler = event => (
     this.setState({ newMessageText: event.target.value })
   )
 
-  onNewMessageAddClickHandler = () => {}
+  onNewMessageAddClickHandler = () => {
+    dbMessagesRef.push({
+      text: this.state.newMessageText,
+      timestamp: Date.now()
+    })
+    this.setState({ newMessageText: '' })
+  }
 
   render() {
     return (
       <div>
-        <NewMessageForm 
+        <NewMessageForm
           newMessageText={this.state.newMessageText}
           onNewMessageTextChangeHandler={this.onNewMessageTextChangeHandler}
           onNewMessageAddClickHandler={this.onNewMessageAddClickHandler}
